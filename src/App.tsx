85b39c73e9177f5a9f22a4d835ddc7f5b189eb18
JulianOviedo <julianovie234@gmail.com>
2023-07-12 19:42:56 -0300

    add cards below shortsurls

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BurgerMenuIcon, IlustrationWorking, Logo } from './components/Icons'
 import { ShortUrls, getShortUrl } from './services/getShortUrl'
 import { toast } from 'react-hot-toast'
 import { ShortUrlModal } from './components/ShortUrlModal'
+import { BenefitCard } from './components/BenefitsCard'
 
 function App () {
   const inputRef = useRef<HTMLInputElement>(null)
@@ -58,7 +59,7 @@ function App () {
           <button onClick={handleGetStarted} className='bg-cyan-500 text-white p-4 px-20 rounded-full mt-6 cursor-pointer active:bg-cyan-200'>Get Started</button>
         </div>
         <section className='relative flex flex-col justify-center items-center p-4 mt-10 bg-violet-950 rounded-xl mx-6 h-40 gap-4'>
-          <img className='absolute top-0 right-0' src='../public/icons/bg-shorten-mobile.svg' alt='stain' />
+          <img className='absolute top-0 right-0' src='/icons/bg-shorten-mobile.svg' alt='stain' />
           <div className='z-10'>
             <input ref={inputRef} className='rounded p-4 w-full mb-2' type='text' placeholder='Shorten a link here...' />
             <button onClick={handleShortenLink} className=' bg-cyan-500 text-white p-4 w-full rounded cursor-pointer hover:bg-cyan-200'>Shorten it!</button>
@@ -67,10 +68,31 @@ function App () {
         </section>
         {(shortUrls.length > 0) &&
           <ShortUrlModal responseApi={shortUrls} />}
-        <section className='mt-20'>
+        <section className='my-20'>
           <h1 className='font-black text-4xl text-center'>Advanced Statics</h1>
           <p className='text-center text-xl mt-4 text-slate-400'>Track how your links are performing across the web with our advanced statistics dashboard</p>
         </section>
+        <BenefitCard
+          title='Brand Recognition'
+          description='Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.'
+          image='/icons/icon-brand-recognition.svg'
+        />
+        <div className='flex justify-center items-center relative -z-10'>
+          <div className='h-48 w-2 bg-cyan-500 absolute -z-10' />
+        </div>
+        <BenefitCard
+          title='Detailed Records'
+          description='Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.'
+          image='/icons/icon-detailed-records.svg'
+        />
+        <div className='flex justify-center items-center relative -z-10'>
+          <div className='h-48 w-2 bg-cyan-500 absolute -z-10' />
+        </div>
+        <BenefitCard
+          title='Fully Customizable'
+          description='Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.'
+          image='/icons/icon-fully-customizable.svg'
+        />
       </main>
     </>
   )
---
+++ src/components/BenefitsCard.tsx
@@ -0,0 +1,20 @@
+interface BenefitCardProps {
+  title: string
+  description: string
+  image: string
+
+}
+
+export const BenefitCard = ({ title, description, image }: BenefitCardProps) => {
+  return (
+    <div className='flex justify-center items-center mb-20'>
+      <article className='w-80 h-80 flex flex-col  rounded bg-white-200 justify-center items-center p-6 text-center bg-slate-50'>
+        <div className='bg-violet-950 rounded-full w-20 h-20 flex justify-center items-center -mt-24'>
+          <img src={image} alt='Benefício' />
+        </div>
+        <h1 className='font-extrabold text-2xl text-center mt-8'>{title}</h1>
+        <p className='text-center text-lg mt-4 text-slate-400'>{description}</p>
+      </article>
+    </div>
+  )
+}
